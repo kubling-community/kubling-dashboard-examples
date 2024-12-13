2f800e0b7068d4f583cfa3ab991740fa62a13669
isoubelet <isoubelet@bluelone.com>
2024-12-13 12:48:46 +0100

    - Compressed the Superset DB file to reduce repository size and improve pull times. - Note: This change requires `unzip` to be available locally for setup.

--- kubling-metrics/run-compose.sh
+++ kubling-metrics/run-compose.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 sh gen-bundles.sh
+unzip -o ./superset_home/superset.zip superset.db -d ./superset_home
 SECRET_KEY="WMq6YJfpeYEytXEs8P2NN8sX/awDASKfx6wGBDUi+YRbIfQqhYaQM5lD" docker compose up
